c863c939102b9291ef7a93b381371dd0688b9a6c
suraj <surajnakhale2407@gmail.com>
2026-03-24 23:04:58 +0530

    added db logic in http-server

--- apps/https-server/src/index.ts
+++ apps/https-server/src/index.ts
@@ -11,9 +11,6 @@ const app = express();
 
 app.use(express.json())
 
-// Mock data storage
-const users: any[] = [];
-const rooms: any[] = [];
 
 //signup endpoint
 app.post("/signup", async (req, res) => {
@@ -35,6 +32,7 @@ app.post("/signup", async (req, res) => {
     
     try{
         console.log("DB URL =", process.env.DATABASE_URL)
+        //db call to create new user in users table
         const user = await prisma.user.create({
             data: {
                 name: data.name,
@@ -53,10 +51,12 @@ app.post("/signup", async (req, res) => {
         })
 
     }
-    catch(err){
+    catch(err: any){
         res.status(500).json({
             message: "error creating user",
-            error: err
+            error: err.message,
+            code: err.code,
+            meta: err.meta
         })
     }
 
@@ -65,74 +65,120 @@ app.post("/signup", async (req, res) => {
 
 //signin endpoint
 app.post("/signin", async (req, res) => {
-    const parsed = signinSchema.safeParse(req.body);
 
-    if(!parsed.success){
-        res.json({
-            message: "invalid input!!"
-        })
-        return;
+    try{
+        const parsed = signinSchema.safeParse(req.body);
+    
+        if(!parsed.success){
+            res.status(400).json({
+                message: "invalid input!!"
+            })
+            return;
+        }
+    
+        let data = parsed.data
+    
+        //database call to check the user present or not
+        let user = await prisma.user.findUnique({
+            where: {
+                username: data.username
+            }
+        });
+    
+        if(!user){
+            res.status(401).json({
+                message: "user does not exist"
+            })
+            return;
+        }
+    
+        let passMatch = await bcrypt.compare(data.password, user.password)
+    
+        if(!passMatch){
+            res.status(401).json({
+                message: "incorrect password"
+            })
+            return;
+        }
+    
+        if(passMatch){
+            let token = jwt.sign({
+                userId: user.id
+            }, 
+            JWT_SECRET, 
+            { expiresIn: "7d" }
+        )
+    
+            return res.json({
+                token,
+                message: "signin successfully"
+            });
+        }
+
+    }
+    catch(err){
+        return res.status(500).json({
+            message: "Internal server error"
+        });
     }
 
-    let data = parsed.data 
-
-    let user = users.find(u => u.username === data.username);
-
-    if(!user){
-        res.json({
-            message: "user does not exist pls signup first!!!!"
-        })
-        return;
-    }
-
-    let passMatch = await bcrypt.compare(data.password, user.password)
-
-    if(!passMatch){
-        res.json({
-            message: "incorrect password pls try again!!"
-        })
-        return;
-    }
-
-    if(passMatch){
-        let token = jwt.sign({
-            userId: user.id
-        }, JWT_SECRET)
-
-        res.json({
-            token: token,
-            message: "signin successfully"
-        })
-    }
 })
 
 
 
 //create room endpoint
 app.post("/create-room", authMiddleware, async (req, res) => {
-    const parsed = roomSchema.safeParse(req.body);
-    //which user created the room 
-    const userId = (req as any).userId;
-    const data = parsed.data;
+    try{
+            //which user created the room 
+        const userId = (req as any).userId;
 
-      if (!userId) {
+        if (!userId){
         return res.status(401).json({
             message: "Unauthorized"
         });
+        }
+
+        const parsed = roomSchema.safeParse(req.body);
+
+        if (!parsed.success) {
+            return res.status(400).json({
+                message: "Invalid input"
+            })
+        }
+
+        const { slug } = parsed.data;
+
+        
+        const existing = await prisma.room.findUnique({
+            where: { 
+                slug 
+            }
+        })
+
+            if (existing) {
+                return res.status(409).json({
+                    message: "Room already exists"
+                })
+            }
+
+        const room = await prisma.room.create({
+            data: {
+                slug,
+                adminId: userId
+            }
+        })
+    
+        return res.json({
+            message: "room created successfully",
+            roomId: room.id
+        })
+
     }
-
-    const room = {
-        id: `room_${Date.now()}`,
-        slug: data!.slug,
-        adminId: userId,
-        createdAt: new Date()
-    };
-    rooms.push(room);
-
-    res.json({
-        message: "room created successfully",
-        roomId: room.id
-    })
+    catch(err){
+        return res.status(500).json({
+            message: "Ineternal Server error"
+        })
+    }
 })
 
 
